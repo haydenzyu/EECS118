6caabd5e6b552648cc037d51c2be594577e189f5
Hayden Yu <49046121+haydenzyu@users.noreply.github.com>
2019-12-02 21:59:43 -0800

    working properly

--- GraphProblemSolver/function.py
+++ GraphProblemSolver/function.py
@@ -1,3 +1,6 @@
+#   Hayden Yu, Sienna Ballot    #
+#   EECS 118 Graph Term Project #
+#   December 5, 2019            #
 # 11. find s where is_path(s, A, B) and color(s, Color, t) and t>C
 # C = user input, t = number of edges that has the Color k
 
@@ -6,8 +9,8 @@ import networkx as nx
 def is_path(s, a, b): #returns true if s is a path between node a and node b
     #s is a set of edges
     #a is the starting point, b is end point
-    #find starting point 'a' [a, x] trace until [y, b]
-    arr = [] 
+    #starting point 'a' [a, x] trace until [y, b]
+    arr = []
     return check_path(s, a, b, arr)
 
 def check_path(s, a, b, arr):
@@ -53,21 +56,21 @@ def color(s, k, n): #returns true if number of edges of color k in s is n
         return False
 
 def find_paths(s, a, b, l, l_c, input_color, C, cnt, a_1):
-    adj_nodes = list(s.neighbors(a))
+    adj_nodes = list(s.neighbors(a)) #find adjacent nodes to node a
     #print(a+"'s adjacent nodes"+str(adj_nodes))
-    if adj_nodes:
-        for i in adj_nodes:
+    if adj_nodes: #if it has adjacent nodes
+        for i in adj_nodes: 
             #print(i)
-            if i == b:
+            if i == b: #the adjacent node is b
                 l.append(i)
                 l_c.append(s[a][i]['color'])
                 #print("num of input color: "+str(l_c.count(input_color)))
-                if l_c.count(input_color) > C:
+                if l_c.count(input_color) > C: #t > C
                     f = open("output.txt", "a+")
                     f.write("path_%d:\n" %cnt)
                     cnt += 1
                     arr = []
-                    for j in range(len(l)-1):
+                    for j in range(len(l)-1): #create a set of edges, write to output
                         tup = (l[j],l[j+1],l_c[j])
                         arr.append(tup)
                         f.write(l[j]+", "+l[j+1]+"\n")
@@ -76,11 +79,14 @@ def find_paths(s, a, b, l, l_c, input_color, C, cnt, a_1):
                     print("is_path() is "+str(is_path(arr, a_1, b)))
                     l.remove(i)
                     l_c.remove(s[a][i]['color'])
+                else:
+                    l.remove(i)
+                    l_c.remove(s[a][i]['color'])
                 return cnt
     
-            elif i in l:
+            elif i in l: #the adjacent node is already visited
                 continue
-            elif i not in l:
+            elif i not in l: #the adjacent node hasn't been visited
                 l.append(i)
                 #print("add color: "+s[a][i]['color'])
                 l_c.append(s[a][i]['color'])
@@ -88,38 +94,9 @@ def find_paths(s, a, b, l, l_c, input_color, C, cnt, a_1):
                 cnt = find_paths(s, x, b, l, l_c, input_color, C, cnt, a_1)
                 l.remove(i)
                 l_c.remove(s[a][i]['color'])
-            else:
+            else: 
                 print("no such path")
                 break
         else:
             #print("dead end")
             return cnt
-'''
-def total_weight(s, n):
-    for i in s:
-        total_weight += i['weight']
-
-    if total_weight == n:
-        return True
-    else:
-        return False
-
-def no_nodes(s, n):
-    nodes = []
-    for i in s: #finds unique nodes in set of edges, 's'
-        if i[0] in nodes:
-            continue
-        else:
-            nodes.append(i[0])
-
-        if i[1] in nodes:
-            continue
-        else:
-            nodes.append(i[1])
-
-    if len(nodes) == n:
-        return True
-    else:
-        return False
-'''
-
